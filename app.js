ae7a209eba1d9feef5fa7d98a58fe925fabf8cde
Josmar Guadarrama <al.221710905@gmail.com>
2024-07-02 21:52:20 -0600

    se regresa el archivo app.js a la versión de juego de adivina un numero

--- app.js
+++ app.js
@@ -1,2 +1,18 @@
  //inicio de codigo, alta de variables
-let 
+ numeroSecreto = 6;
+let numeroDeUsuario = prompt("dame un número:");
+/*
+monitoreo de ingreso de dato
+*/
+ console.log (numeroDeUsuario);
+/*
+condicionante del juego 
+*/
+ if (numeroDeUsuario == numeroSecreto){
+    alert(`acertaste, el número es: ${numeroDeUsuario}`);
+    mensajeDeBienvenida = ('Bienvenido a la app');
+    alert(mensajeDeBienvenida);
+ } else {
+    alert ('Lo siento, No acertaste');
+ }
+ 
