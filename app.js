0d35628254eb96123e0f7fa5a2e818fd11d4b578
Josmar Guadarrama <al.221710905@gmail.com>
2024-07-04 20:17:26 -0600

    se actualiza código para hacer aleatorio la opción mayor de número secreto

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
- //inicio de codigo, alta de variables
+//inicio de codigo, alta de variables
 let numeroSecreto = Math.floor(Math.random()*10+1); // se agregó número aleatorio
 let numeroDeUsuario = 0;
 let intentos = 1;
 // let palabraIntentos = 'intento'; (se eliminó la variable por operador ternario)
 let maximoDeIntentos = 3;
+let numeroMenor = 1; 
+let numeroMayor = Math.floor(Math.random()*40)+4; 
 //condicionante del juego 
 
 while (numeroDeUsuario != numeroSecreto){
- numeroDeUsuario = parseInt (prompt("dame un número entre 1 y 10:"));
+ numeroDeUsuario = parseInt (prompt(`dame un número entre 1 y ${numeroMayor}:`));
  console.log (numeroDeUsuario);
  
       if (numeroDeUsuario == numeroSecreto){
